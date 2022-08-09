75216aff652e95b8ea8ca7da1830ed2d90d00245
Yixin Peng <ypeng27@hawk.iit.edu>
2022-08-08 19:09:46 -0700

    Added jsq with building number

--- fetch/fetch18Park.py
+++ fetch/fetch18Park.py
@@ -3,7 +3,7 @@ from fetch.fetch981Management import Fetch981Management
 
 class Fetch18Park(Fetch981Management):
     def __init__(self, driver, browser):
-        super().__init__(driver,  browser)
+        super().__init__(driver, browser)
         self.room_type_map = [
             ("^1 bed 1 bath$", "1B1B"),
             ("^Townhouse$", "2B1.5B"),
--- fetch/fetchJsq.py
+++ fetch/fetchJsq.py
@@ -8,7 +8,7 @@ from fetch.fetch import Fetch
 
 class FetchJsq(Fetch):
     def __init__(self, driver, browser):
-        super().__init__(driver,  browser)
+        super().__init__(driver, browser)
         self.room_type_map = [
             ("^One Bed/ One Bath - .*$", "1B1B"),
             ("^One Bed/ One Bath/ Plus - .*$", "1B1BD"),
@@ -23,15 +23,19 @@ class FetchJsq(Fetch):
         self.web_wait.until(
             EC.presence_of_element_located((By.XPATH, '//table[@id="table"]/tbody/tr'))
         )
-        room_list = self.driver.find_elements(by=By.XPATH, value='//table[@id="table"]/tbody/tr')
-        for row in room_list:
-            if not row.text:
-                # FIXME: more room will show after click GO, this might not be stable
-                continue
-            room = row.find_elements(by=By.XPATH, value=".//td")
-            self.add_room_info(
-                room_number=room[1].text,
-                room_type=room[2].text,
-                move_in_date=room[5].text,
-                room_price=room[3].text,
+        for building_idx in range(1, 3):
+            room_list = self.driver.find_elements(
+                by=By.XPATH,
+                value=f'//table[@id="table"]/tbody/tr[contains(@class,"tower-{building_idx}")]',
             )
+            for row in room_list:
+                if not row.text:
+                    # FIXME: more room will show after click GO, this might not be stable
+                    continue
+                room = row.find_elements(by=By.XPATH, value=".//td")
+                self.add_room_info(
+                    room_number=f"{room[1].text} - JD{building_idx}",
+                    room_type=room[2].text,
+                    move_in_date=room[5].text,
+                    room_price=room[3].text,
+                )
