6440f74d436587fa40ec96c02232da0d5ff7284e
Yixin Peng <ypeng27@hawk.iit.edu>
2023-08-07 18:48:38 -0700

    fixed Veris

--- fetch/fetchVeris.py
+++ fetch/fetchVeris.py
@@ -34,67 +34,27 @@ class FetchVeris(Fetch):
             button.click()
 
     def fetch_web(self):
-        def read_list():
-            if not self.reading_list:
-                self.slow_button_click(view_buttons[0])
-                sleep(0.5)
-                self.reading_list = True
-            for room in rooms:
-                room_classes = room.get_attribute("class").split(" ")
-                if room_classes[1] == "wpgb-card-1":
-                    room_type = room.find_element(
-                        by=By.XPATH, value='.//div[@class="wpgb-block-10"]'
-                    ).text
-                    room_price = room.find_element(
-                        by=By.XPATH, value='.//div[@class="wpgb-block-8"]'
-                    ).text.replace("From ", "")
-                    move_in_date = room.find_element(
-                        by=By.XPATH, value='.//div[@class="wpgb-block-1"]/div[1]'
-                    ).text
-                    rooms_dict[room_classes[2]] = {
-                        **rooms_dict[room_classes[2]],
-                        "room_price": room_price,
-                        "room_type": room_type,
-                        "move_in_date": move_in_date,
-                    }
-
-        def read_floor():
-            if self.reading_list:
-                self.slow_button_click(view_buttons[1])
-                sleep(0.5)
-                self.reading_list = False
-            for room in rooms:
-                room_classes = room.get_attribute("class").split(" ")
-                if room_classes[1] == "wpgb-card-16":
-                    room_number = room.find_element(
-                        by=By.XPATH, value=".//h3[contains(@class, 'wpgb-block-1')]/a[1]"
-                    ).text
-                    rooms_dict[room_classes[2]] = {
-                        **rooms_dict[room_classes[2]],
-                        "room_number": room_number,
-                    }
-
         self.get_url_with_retry(self.url)
         rooms_dict = defaultdict(dict)
         cur_page = 1
-        self.reading_list = True
         while True:
-            rooms = self.wait_until_xpath("//article[contains(@class, 'wpgb-card')]")
-            view_buttons = self.driver.find_elements(
-                by=By.XPATH,
-                value='//div[@class="uagb-button__link"]',
-            )
-            if self.reading_list:
-                read_list()
-                read_floor()
-            else:
-                read_floor()
-                read_list()
+            rooms = self.wait_until_xpath("//div[@class='omg-results-card bg-white']")
+            for room in rooms:
+                room_info = room.find_elements(
+                    by=By.XPATH,
+                    value='.//div[contains(@class, "omg-results-card-body-element px-4")]',
+                )
+                self.add_room_info(
+                    room_number=room_info[1].text,
+                    room_type=room_info[2].text,
+                    room_price=room_info[3].text.replace("From ", ""),
+                    move_in_date=room_info[4].text.split("\n")[0],
+                )
             cur_page += 1
             try:
                 next_page_button = self.driver.find_element(
                     by=By.XPATH,
-                    value=f'//a[@aria-label="Goto Page {cur_page}" and @data-page="{cur_page}" and text()="{cur_page}"]',
+                    value=f'//span[@data-page="{cur_page-1}" and text()="{cur_page}"]',
                 )
             except Exception:
                 break
