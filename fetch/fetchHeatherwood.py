3c0297b339950294d344fe3e5ecf9c7be5b6557f
Yixin Peng <ypeng27@hawk.iit.edu>
2023-12-12 22:38:00 -0800

    fixed heatherwood

--- fetch/fetchHeatherwood.py
+++ fetch/fetchHeatherwood.py
@@ -12,20 +12,21 @@ class FetchHeatherwood(Fetch):
         if self.check_availability():
             logging.info(f"No room available in {self.website_name}, skipping...")
             return
-        # TODO: change to //a|//button
-        try:
-            apply_buttons = self.wait_until_xpath('//a[contains(@class, "btn-viewDetails")]')
-        except Exception as err:
-            logging.info(f"No room available in {self.website_name}, skipping...{err}")
-            return
+        attr_filter = 'contains(@class, "btn-viewDetails")'
+        apply_buttons = self.wait_until_xpath(
+            f"//a[{attr_filter}]|//button[{attr_filter}]"
+        )
         for index in range(len(apply_buttons)):
             self.fetch_room_info(index)
 
     def fetch_room_info(self, index):
-        apply_button = self.wait_until_xpath(f'//a[@data-selenium-id="FPButton_{index}"]')[0]
-        if "contact us" in apply_button.text.lower():
+        attr_filter = f'@data-selenium-id="FPButton_{index}"'
+        apply_button = self.wait_until_xpath(
+            f"//a[{attr_filter}]|//button[{attr_filter}]"
+        )[0]
+        if "contact" in apply_button.text.lower():
             return
-        self.move_to_center(apply_button)
+        self.move_to_element(apply_button)
         apply_button.click()
         self.driver.switch_to.window(self.driver.window_handles[0])
         self.fetch_room()
@@ -33,7 +34,9 @@ class FetchHeatherwood(Fetch):
         self.driver.switch_to.window(self.driver.window_handles[0])
 
     def fetch_room(self):
-        self.web_wait.until(EC.presence_of_element_located((By.XPATH, "//table/tbody/tr")))
+        self.web_wait.until(
+            EC.presence_of_element_located((By.XPATH, "//table/tbody/tr"))
+        )
         room_type = self.driver.find_elements(
             by=By.XPATH, value='//div[contains(@id,"other-floorplans")]'
         )[0].text.split("-")[-1]
@@ -48,14 +51,19 @@ class FetchHeatherwood(Fetch):
             current_url = self.driver.current_url
             self.web_wait.until(
                 EC.presence_of_element_located(
-                    (By.XPATH, f'//input[@data-selenium-id="btnUnitSelect{str(room_idx + 1)}"]')
+                    (
+                        By.XPATH,
+                        f'//input[@data-selenium-id="btnUnitSelect{str(room_idx + 1)}"]',
+                    )
                 )
             ).click()
             self.driver.switch_to.window(self.driver.window_handles[0])
             move_in_date = self.web_wait.until(
                 EC.presence_of_element_located((By.ID, "sMoveInDate"))
             ).get_attribute("value")
-            room_price = self.wait_until_xpath('//div[@id="divPricingInfo"]/div/div/label')[-1].text
+            room_price = self.wait_until_xpath(
+                '//div[@id="divPricingInfo"]/div/div/label'
+            )[-1].text
             self.add_room_info(
                 room_number=room_number,
                 room_type=room_type,
