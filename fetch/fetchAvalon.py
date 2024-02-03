cd971c1a09f6f8b6e403a8b0555a3be43e136779
Yixin Peng <ypeng27@hawk.iit.edu>
2024-02-02 18:08:57 -0800

    Updated Avalon

--- fetch/fetchAvalon.py
+++ fetch/fetchAvalon.py
@@ -9,30 +9,26 @@ from utils.utils import find_closing_bracket
 class FetchAvalon(Fetch):
     def fetch_web(self):
         self.get_url_with_retry(self.url)
-        scripts = self.driver.find_elements(by=By.XPATH, value='.//script[@id="fusion-metadata"]')
+        scripts = self.driver.find_elements(
+            by=By.XPATH, value='.//script[@id="fusion-metadata"]'
+        )
         script_text = scripts[0].get_attribute("innerHTML")
         start_index = script_text.find('"units"') + 8
         end_index = find_closing_bracket(script_text, start_index)
         rooms = json.loads(script_text[start_index : end_index + 1])
         for room in rooms:
             # Process price
-            if room.get("lowestPricePerMoveInDate"):
-                price = room.get("lowestPricePerMoveInDate")
-                gross_price, net_price = price["price"], price["netEffectivePrice"]
-            elif room.get("pricesPerMoveinDate"):
-                gross_price, net_price = 999999, 999999
-                for price in room["pricesPerMoveinDate"]:
-                    for price1 in price["pricesPerTerms"].values():
-                        if price1["netEffectivePrice"] < net_price:
-                            gross_price = price1["price"]
-                            net_price = price1["netEffectivePrice"]
-            final_price = f"${net_price}"
-            if net_price != gross_price:
-                final_price = f"N${net_price} G${gross_price}"
+            price = room["startingAtPricesUnfurnished"]["prices"]
+            gross_price, net_price = price["price"], price["netEffectivePrice"]
+            final_price = (
+                f"N${net_price} G${gross_price}"
+                if net_price != gross_price
+                else f"${net_price}"
+            )
 
             # Process room type
-            bedroom = room["bedroom"]
-            bathroom = room["bathroom"]
+            bedroom = room["bedroomNumber"]
+            bathroom = room["bathroomNumber"]
             room_type = f"{bedroom}B{bathroom}B"
             if bedroom == 0:
                 room_type = "Studio"
@@ -42,10 +38,16 @@ class FetchAvalon(Fetch):
             room_url_suffix = "-".join([room_unit_id[1]] + room_unit_id[1:])
             room_url = f"{self.url}/apartment/{room_url_suffix}"
 
+            # Process date
+            date_string = room["startingAtPricesUnfurnished"]["moveInDate"]
+            year = date_string[:4]
+            month = date_string[5:7] if date_string[5] != "0" else date_string[6]
+            day = date_string[8:10] if date_string[8] != "0" else date_string[9]
+
             self.add_room_info(
-                room_number=room["name"],
+                room_number=room["unitName"],
                 room_type=room_type,
-                move_in_date=room["availableDate"].split(" ")[0],
+                move_in_date=f"{month}/{day}/{year}",
                 room_price=final_price,
                 room_url=room_url,
             )
